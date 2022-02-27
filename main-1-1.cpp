fde68e97172fdb3e26ba3463ff55374b55675fe7
Gerald Freislich <freislichg@gmail.com>
2022-02-27 19:49:19 +1030

    commit on Week 0

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "function-1-1.cpp"
 
-extern void hello();
+// question: What's the point of saying extern void and not just including the file to link to
+// I ask because I had an issue with the test submission where the compiler failed to compile because of multiple definitions of the 'hello' function
+// extern void hello();
 
 int main(){
     hello();
