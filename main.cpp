7230a336e043d13c598e9cc5840385bf3dcdfa26
Gerald Freislich <freislichg@gmail.com>
2022-03-01 10:26:47 +1030

    commit on Week-0

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include "count.cpp"
+using namespace std;
+
+int main(){
+    int arg[5] = {3, 5, 7, 9, 11};
+    // count(arg, 5);
+    cout << "The total number of evens is: "<< count(arg, 5) << endl;
+    return 0;
+}
+//he
+
