69852c019c4f54c3af30358d981e8339bb448200
Gerald Freislich <freislichg@gmail.com>
2022-03-02 10:09:46 +1030

    commit on Week-0

--- Week-1/Medium Challenges/function-4-4.cpp
+++ Week-1/Medium Challenges/function-4-4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool ascending(int array[], int n){
     int previous_num = 0;
     if(n < 0){
-        cout << "The number of integers is less than 1" << endl;
+        cout << "The number of integers is less than 0" << endl;
     }
     else{
         for(int i=0; i < n; i++){
