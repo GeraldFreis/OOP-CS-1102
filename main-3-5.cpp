118576cee8337cd383da2afe4f0b5b2283d67128
Gerald Freislich <freislichg@gmail.com>
2022-03-03 13:21:19 +1030

    commit on Week-0

--- main-3-5.cpp
+++ main-3-5.cpp
@@ -5,12 +5,12 @@ using namespace std;
 extern double sumeven(double[], int);
 
 int main(int argc, char **argv){
-    int arr[argc];
+    double arr[argc];
 
     for(int i =0; i < argc; i++){
         arr[i] = *(argv[i]);
     }
-    
+
     sumeven(arr, argc);
 
     return 0;
