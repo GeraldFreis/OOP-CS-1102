b46c6a9f5e986298fc285dba8c2b2c42a53f4bf0
Gerald Freislich <freislichg@gmail.com>
2022-03-03 14:21:13 +1030

    commit on Week-0

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -4,20 +4,20 @@
 #include <stdio.h>
 using namespace std;
 
-#include <stdio.h>
-
 int median(int array[], int n){
     int middle_of_array = n / 2;
     int median = 0;
 
     // sorting the array
     sort(array, array+n);
-
+    
+    // checking whether the number of elements is less than zero or an even number
     if(n < 0 || n % 2 == 0){
         return 0;
     }
 
     else{
+        
         median = array[middle_of_array];
         return median;
     }
