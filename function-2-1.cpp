a5692fcb23bb310e65fd7aee32bfe9b6c3fbb3e1
Gerald Freislich <freislichg@gmail.com>
2022-03-03 13:36:09 +1030

    commit on Week-0

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -10,14 +10,11 @@ int minimum(int array[], int n){
     
     else{
         for(int i=0; i< n; i++){
-            if(i==0){
-                smallest_num += abs(array[i]);
+  
+            if(array[i] <= smallest_num)
+            {
+                smallest_num = array[i];
             }
-            else{
-                if(array[i] <= smallest_num){
-                    smallest_num = array[i];
-                }
             }
         }
     }
-}
