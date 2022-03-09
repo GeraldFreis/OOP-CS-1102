a8c212826aefc8934e3a230468649ed306fb85a2
Gerald Freislich <freislichg@gmail.com>
2022-03-09 23:04:50 +1030

    commit on Week-2

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,12 @@ using namespace std;
 void print_scaled_matrix(int array[3][3], int scale){
     for(int row = 0; row < 3; row++){
         for(int column = 0; column < 3; column++){
-            cout << array[row][column]*scale << " ";
+            if(column != 2){
+                cout << array[row][column]*scale << " ";
+            }
+            else{
+                cout << array[row][column]*scale;
+            }
         }
         cout << endl;
     }
