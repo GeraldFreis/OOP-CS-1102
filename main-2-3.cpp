4eee3cbc3fb8ff8c5baf5d01eec0bf70613141bf
Gerald Freislich <freislichg@gmail.com>
2022-03-09 23:15:15 +1030

    commit on Week-2

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "function-2-3.cpp"
+// #include "function-2-3.cpp"
 using namespace std;
 
 extern int sum_elements(int [], int);
 
-int main(){
-    int array[5] = {1, 2, 0, 2, 1};
-    cout << sum_elements(array, 5) << endl;
+int main(int array[], int size){
+    // int array[5] = {1, 2, 0, 2, 1};
+    cout << sum_elements(array, size) << endl;
 }
