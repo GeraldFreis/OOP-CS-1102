b429231b2b20bc4ed4f34f1de3b4af52971184ed
Gerald Freislich <freislichg@gmail.com>
2022-03-09 22:57:47 +1030

    commit on Week-2

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "function-1-3.cpp"
+// #include "function-1-3.cpp"
 using namespace std;
 
-extern void count_numbers(int []);
+extern void count_numbers(int [4][4]);
 
-int main(){
-    int matrix[4][4] = {{18, 2, 3, 4},
-                        {1, 23, 0, 4},
-                        {-1, 100, 3, 4},
-                        {1, 2, 3, 9}};
+int main(int matrix[4][4]){
+    // int matrix[4][4] = {{18, 2, 3, 4},
+    //                     {1, 23, 0, 4},
+    //                     {-1, 100, 3, 4},
+    //                     {1, 2, 3, 9}};
     count_numbers(matrix);
     return 0;
 }
