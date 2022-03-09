e408641820d3201d5ebd9ce460a31e8fdc2b948f
Gerald Freislich <freislichg@gmail.com>
2022-03-09 23:08:46 +1030

    Commit on Week-2

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -6,9 +6,13 @@ void print_summed_matrices(int array_1[3][3], int array_2[3][3]){
     for(int row = 0; row < 3; row++){
         for(int column = 0; column < 3; column++){
             int sum = array_1[row][column] + array_2[row][column];
-            cout << sum << " ";
+            if(column != 2){
+                cout << sum << " ";
+            }
+            else{
+                cout << sum << endl;
+            }
         }
-        cout << endl;
     }
 
 }
