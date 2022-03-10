2d3f97ebd9932145a4b6a4dd54a9bf00fe617b1f
Gerald Freislich <freislichg@gmail.com>
2022-03-10 15:13:48 +1030

    commit on Week-2

--- Week-2/Practical/main-2-2.cpp
+++ Week-2/Practical/main-2-2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-// #include "function-2-2.cpp"
+#include "function-2-2.cpp"
 using namespace std;
 
 extern int binary_to_number(int [], int);
 
-int main(int array[], int n){
-    // int array[8] = {0,1,1,0,0,0,1,0};
-    cout << binary_to_number(array, n) << endl;
+int main(){
+    int array[8] = {0,1,1,0,0,0,1,0};
+    cout << binary_to_number(array, 8) << endl;
 }
