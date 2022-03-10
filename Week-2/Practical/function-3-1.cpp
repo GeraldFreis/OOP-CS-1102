8641af2a40c35ddf7401b3ea810f4b443d5298ba
Gerald Freislich <freislichg@gmail.com>
2022-03-10 14:10:23 +1030

    commit on Week-2

--- Week-2/Practical/function-3-1.cpp
+++ Week-2/Practical/function-3-1.cpp
@@ -4,3 +4,9 @@ using namespace std;
 // customer[0] in queue pays the price of their items (products):
 // customer[0]'s product_cost=money_paid;
 
+// int customer_que(const int* &customers[], const int &num_of_customers){
+//     int *customer_array[num_of_customers];
+//     customer_array = new int;
+//     customer_array = customers;
+// }
+
