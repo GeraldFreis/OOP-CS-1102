5bb4dd970aa4db68548e6bf472d3c66a24f12fa9
Gerald Freislich <freislichg@gmail.com>
2022-03-15 10:04:10 +1030

    commit on Week3

--- Week-3/Workshop/main-1-3.cpp
+++ Week-3/Workshop/main-1-3.cpp
@@ -8,14 +8,21 @@ extern void print_class(std::string [4], std::string [], int [][4], int);
 int main(){
     std::string courses[4] = {"Maths", "Algebra", "Programming", "English"};
     int num_of_students;
-    cin >> "Please enter a number of students: " >> num_of_students >> endl;
-    int grades[][4];
+    cout << "Please enter a number of students: ";
+    cin >> num_of_students;
+    cout << endl;
+    int grades[num_of_students][4];
     std::string students[num_of_students];
-    for(int i = 0; i < n; i++){
-        cin >> "Enter a student name: " >> endl;
+    for(int i = 0; i < num_of_students; i++){
+        cout << "Enter a student name: ";
+        cin >> students[i];
+        cout << endl;
         for(int j = 0; j < 3; j++){
-            cin >> "Enter the grade for this student: " >> grades[i][j] >> endl;
+            cout << "Enter the grade for this student: ";
+            cin >> grades[i][j];
+            cout << endl;
         }
     }
+    print_class(courses, students, grades, num_of_students);
     return 0; 
 }
