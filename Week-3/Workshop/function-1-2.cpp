a33b18e9ac3e1ddfa7c57ce115809692bc86508d
Gerald Freislich <freislichg@gmail.com>
2022-03-15 09:33:58 +1030

    commit on Week3

---
+++ Week-3/Workshop/function-1-2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void print_class(std::string courses[4], std::string students[], int report_card[][4], int n){
+    for(int i = 0; i < n; i++){
+
+    }
+}
