0b8dc290ade7dc168d56288247383577dc30f675
Gerald Freislich <freislichg@gmail.com>
2022-03-15 10:31:21 +1030

    commit on Week3

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void printer(int array[10][10]){
     for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 9 ; j++){
+        for(int j = 0; j < 10 ; j++){
             cout << array[i][j] << " ";
         }
         cout << endl;
--- function-1-2.cpp
+++ function-1-2.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 void print_class(std::string courses[4], std::string students[], int report_card[][4], int n){
     for(int i = 0; i < n+1; i++){
-        cout << "report card" << courses[0] << " "<< courses[1] << " " << courses[2] << " " << courses[3] << " " << endl;
+        cout << "report card " << courses[0] << " "<< courses[1] << " " << courses[2] << " " << courses[3] << " " << endl;
+        cout << students[i] << " ";
         for(int j = 0; j < 3; j++){
-            cout << students[i][j] << " ";
+            cout << report_card[i][j] << " ";
         }
         cout << endl;
     }
