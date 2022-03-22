6938f402fa2a7de7262a390499d6b5fe554570bd
Gerald Freislich <freislichg@gmail.com>
2022-03-22 10:33:52 +1030

    commit on Week3

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+// #include "function-2-3.cpp"
+#include <string>
 using namespace std;
 
+float arithmetic_ops(float, float, string);
+
 int main(){
-    int array[4] = {1, 2, 3, 4};
-    
+    float left;
+    float right;
+    string op;
+
+    cout << "please enter your name: " << endl;
+    cin >> op;
+    cout << "Enter a float" << endl;
+    cin >> left;
+    cout << "Enter another:" << endl;
+    cin >> right;
+    arithmetic_ops(left, right, op);
+    return 0;
+    // delete left, right, op;
+}
