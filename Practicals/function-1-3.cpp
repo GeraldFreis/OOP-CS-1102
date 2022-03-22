4e44e2f5ffde173fda5d2cc4ab0acd8a13e96f6c
Gerald Freislich <freislichg@gmail.com>
2022-03-22 18:16:02 +1030

    commit on Week4

---
+++ Practicals/function-1-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+void cpyia(int old_array[], int new_array[], int length){
+    // creating the pointers
+    int *oldptr, *newptr;
+    oldptr = &old_array[0];
+    newptr = &new_array[0];
+
+    
+
+}
