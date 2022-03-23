345dcbc1466b61b77627559334daa0405b0da2cd
Gerald Freislich <freislichg@gmail.com>
2022-03-23 18:29:31 +1030

    commit on Week4

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -7,8 +7,8 @@ void cpyda(double *old_array, double *new_array, int length){
     for(int i = 0; i < length; i++){
         new_array[i] = (old_array[i]);
     }
-    for(int i = 0; i < length; i++){
-        cout << new_array[i] << " ";
-    }
-    cout << endl;
+    // for(int i = 0; i < length; i++){
+    //     cout << new_array[i] << " ";
+    // }
+    // cout << endl;
 }
