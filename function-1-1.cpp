d5c6a2c582c95d337bd7f775b172251d2d68c19c
Gerald Freislich <freislichg@gmail.com>
2022-03-29 12:24:46 +1030

    commit on Week5

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -5,14 +5,14 @@ int *readNumbers(){
     int *arr = new int[10];
     // cout << "enter 10 numbers: " << endl;
 
-    for(int i = 0; i < 9; i++){
+    for(int i = 0; i < 10; i++){
         cin >> arr[i];
     }
     return arr;
 }
 
 void printNumbers(int *numbers,int length){
-    for(int i = 0; i < length-1; i++){
+    for(int i = 0; i < length; i++){
         cout << i << " " << numbers[i] << endl;
     }
 }
