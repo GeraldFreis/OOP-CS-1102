0c3f573593aa7ed4308726f107f60580d399a7ac
Gerald Freislich <freislichg@gmail.com>
2022-04-06 16:43:34 +0930

    c

--- meerkat.cpp
+++ meerkat.cpp
@@ -1,9 +1,7 @@
 #include "meerkat.h"
 
 meerkat::meerkat(){
-    age = 0;
-    name = "";
-}
+};
 void meerkat::setName(std::string meerName){name = meerName;}
 void meerkat::setAge(int meerAge){age = meerAge;}
 std::string meerkat::getName(){return name;}
