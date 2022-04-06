a3f0c9dacd2647118d9ec19e0c938e6d12a499ec
Gerald Freislich <freislichg@gmail.com>
2022-04-06 16:45:37 +0930

    commit

--- person.cpp
+++ person.cpp
@@ -6,20 +6,20 @@ person::person(){
 person::person(std::string myName, int Salary){
     name = myName;
     salary = Salary;
-}
+};
 
 std::string person::getName(){
     return name;
-}
+};
 
 int person::getSalary(){
     return salary;
-}
+};
 
 void person::setSalary(int mySalary){
     salary = mySalary;
-}
+};
 void person::setName(std::string myName){
     name = myName;
-}
+};
 
