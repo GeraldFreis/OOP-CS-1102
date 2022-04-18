d9adc21260a3950fa7c5a31b23d6ca0ad3e8692c
Gerald Freislich <freislichg@gmail.com>
2022-04-18 19:57:01 +0930

    Day updates

--- Week-7/src/Days/Weekdays.cpp
+++ Week-7/src/Days/Weekdays.cpp
@@ -1,2 +1,41 @@
 #include "Weekdays.h"
 
+// constructors
+Weekdays::Weekdays(){
+    mintemp = 0;
+    maxtemp = 0;
+    lowerschedbound = 0;
+    upperschedbound = 0;
+    schedule = new int[2];
+};
+
+Weekdays::Weekdays(int min, int max){
+    mintemp = min;
+    maxtemp = max;
+    lowerschedbound = 0;
+    upperschedbound = 0;
+    schedule = new int[2];
+};
+
+
+// member functions
+void Weekdays::settemps(int min, int max){
+    mintemp = min;
+    maxtemp = max;
+};
+
+void Weekdays::calcschedule(){
+    
+};  
+
+void Weekdays::printschedule(){
+    cout << schedule[0] << " " << schedule[1] << "\n"; // no clearing buffers here
+};
+
+int &Weekdays::getschedule(){
+    return schedule[0];
+}
+
+Weekdays::~Weekdays(){
+    delete [] schedule;
+}
--- Week-7/src/Days/Weekdays.h
+++ Week-7/src/Days/Weekdays.h
@@ -1,11 +1,41 @@
 #include <iostream>
-using std::cout; using std::string;
+using std::cout;
+
+/*
+Weekdays class will be the super class of Weekends
+Attrs:
+    int mintemp
+    int maxtemp
+    int lowerschedbound {controls the lower boundary of the schedule}
+    int upperschedbound  {controls the upper boundary of the schedule}
+    int schedule[2] {lower bound is element 1, upper bound is element 2}
+Behaviours
+    settemps {takes mintemp and maxtemp as parameters and assigns them to member vars}
+    calcschedule {takes the set mintemp and maxtemp and produces a upper and lower schedule boundary and then assigns this to the respective element index}
+    getschedule {takes the schedule and returns the address of the array}
+    printschedule {prints the calculated schedule}
+*/
 
 class Weekdays{
     protected:
-        
+        int mintemp;
+        int maxtemp;
+        int lowerschedbound;
+        int upperschedbound;
+        int *schedule; // calculated schedule
+
     public:
-        Weekdays();
-        void calcschedule();
-        ~Weekdays();
+        // constructors
+        Weekdays(); // default constructor
+        Weekdays(int min, int max); // parameterized constructor
+
+        // member functions
+        void settemps(int min, int max); // sets the min and max temp
+        void calcschedule(); // calculates the schedule and assigns it to schedule var
+        int &getschedule(); // returns address of schedule
+        void printschedule(); // prints schedule
+
+
+        // destructor {virtual as its a superclass (we want dynamic binding)}
+        virtual ~Weekdays();
 };
