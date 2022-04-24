53906809430ff01c40c682b8e08d5e9faab98ee7
Gerald Freislich <freislichg@gmail.com>
2022-04-24 10:34:47 +0930

    safe

--- Week-7/src/testingfiles/subfules/testing.cpp
+++ Week-7/src/testingfiles/subfules/testing.cpp
@@ -1,32 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
 using std::cout;
 using std::string;
 
-void calcseason(string &row){
-    // precondition:
-        // passing reference of the row as param
-    // what function does;
-        // changes the fourth element of the row to the season
-    
-    string date = row;
-    date.c_str();
-    // finding the month
-    string month;
-    for(int i = 0; i < date.size(); i++){
-        if(date[i] == '\''){
-            month += date[i+1];
-            month += date[i+2];
-            break;
-        }
-    }
+extern int linenumber(string);
 
-    int monthint = std::stoi(month);
-    
-    // changing the month to its seasonal counterpart
 
-}
 
 class Testing{
     protected:
@@ -36,8 +17,103 @@ class Testing{
     
     public :
         Testing();
+        void calcseason();
         void csvtomatrix();
-        string **getdata();
-        void convertingcsv();
         void printdata();
+        ~Testing();
 };
+
+Testing::Testing(){
+    // default constructor
+
+    filename = "futureshite.csv";
+    numberoflines = linenumber(filename);
+    data_matrix = new string *[numberoflines];
+
+    // creating each of the columns in the matrix
+    for(int i = 0; i < numberoflines; i++){
+        data_matrix[i] = new string[5];
+    }
+}
+
+void Testing::csvtomatrix(){
+    std::ifstream file(filename);
+    std::string row;
+    int index_row = 0;
+
+    while(file.good()){ // while we are still in the range of the file
+
+        // iterating over the elements in the row and capturing them into their respective positions in the matrix
+        for(int i = 0; i < 2; i++){
+            getline(file, data_matrix[index_row][i], ',');
+            // cout << data_matrix[index_row][i] << "\n";
+        }
+
+        getline(file, data_matrix[index_row][2]);
+        index_row++;
+    }
+}
+
+void Testing::calcseason(){
+    // precondition:
+        // passing reference of the row as param
+    // postcondition
+        // changes the fourth element of the row to the season
+    for(int i = 0; i < numberoflines; i++){
+        string date = data_matrix[i][0];
+        
+        // finding the month
+        date.c_str();
+        string month;
+
+        for(int i = 0; i < date.size(); i++){
+            if(date[i] == '/'){
+                month += ((date[i+1]));
+                month += (date[i+2]);
+            }
+        }
+
+        int monthint = stoi(month);
+        
+    //     // changing the month to its seasonal counterpart
+        if(monthint < 3 || monthint >= 11){
+            data_matrix[i][3] = "summer";
+        }
+        else if (monthint >= 3 && monthint < 5){
+            data_matrix[i][3] = "autumn";
+        }
+        else if(monthint >= 5 && monthint < 9)
+        {
+            data_matrix[i][3] = "winter";
+        }
+        else if(monthint >= 9 && monthint < 11){
+            data_matrix[i][3] = "spring";
+        }
+        else{
+            cout << "not in season range" << "\n";
+        }
+    }
+}
+
+void Testing::printdata(){
+    for(int row = 0; row < numberoflines; row++){
+        for(int col = 0; col < 5; col++){
+            cout << data_matrix[row][col] << "\n";
+        }
+    }
+}
+
+Testing::~Testing(){
+    for(int i = 0; i < numberoflines; i++){
+        delete [] data_matrix[i];
+    }
+    delete [] data_matrix;
+}
+
+int main(){
+    Testing testingobject;
+
+    testingobject.csvtomatrix();
+    testingobject.calcseason();
+    testingobject.printdata();
+}
