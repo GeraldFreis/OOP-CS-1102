680c69b6a2cd5a4025135b49d49d2fc8c36a4a2d
Gerald Freislich <freislichg@gmail.com>
2022-04-24 10:44:24 +0930

    updates

--- Week-7/main.cpp
+++ Week-7/main.cpp
@@ -13,21 +13,20 @@ int main(){
     ReadingCSV object(_filename); // initialising the reading csv object
     object.csvtomatrix();
     object.calcseason();
-    object.printdata();
+    // object.printdata();
 
-    // object.printdata(); // printing the data
-    // int numberoflines = linenumber(_filename);
+    int numberoflines = linenumber(_filename);
 
     // // initialising the interpreting csv objects
-    // InterpretingCSV *interpretarray = new InterpretingCSV[numberoflines];
+    InterpretingCSV *interpretarray = new InterpretingCSV[numberoflines];
     
-    // // interpreting the data row by row:
-    // std::string **returnedmatrix = object.getdata();
+    // interpreting the data row by row:
+    std::string **returnedmatrix = object.getdata();
 
-    // for(int i = 0; i < numberoflines; i++){
-    //     interpretarray[i].setdata(returnedmatrix[i][0], returnedmatrix[i][1], returnedmatrix[i][2]);
-    //     interpretarray[i].findingschedule();
-    // }
+    for(int i = 0; i < numberoflines; i++){
+        interpretarray[i].setdata(returnedmatrix[i][0], returnedmatrix[i][1], returnedmatrix[i][2]);
+        interpretarray[i].findingschedule();
+    }
 
-    // delete [] interpretarray;
+    delete [] interpretarray;
 } 
