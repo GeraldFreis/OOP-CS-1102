bc21c67bce691891a2be8684e83f924bff42ab78
Gerald Freislich <freislichg@gmail.com>
2022-04-26 09:26:58 +0930

    updating workshop

--- Workshop/Musician.cpp
+++ Workshop/Musician.cpp
@@ -1,1 +1,20 @@
 #include "Musician.h"
+
+Musician::Musician(){ // a default constructor 
+    instrumentplayed = "";
+    yearsofexperience = 0;
+};
+Musician::Musician(std::string instrument, int experience){ // a constructor that takes the instrument played and the years of experience
+    instrumentplayed = instrument;
+    yearsofexperience = experience;
+};     
+std::string Musician::get_instrument(){    // returns the instrument played
+    return instrumentplayed;
+};
+int Musician::get_experience(){      // returns the number of years experience
+    return yearsofexperience;
+};
+
+Musician::~Musician(){                //A default destructor
+    cout << "destroyed Musician";
+};
--- Workshop/Musician.h
+++ Workshop/Musician.h
@@ -1,2 +1,16 @@
 #include <iostream>
 using std::cout; using std::string;
+
+class Musician{
+    protected:
+        string instrumentplayed;
+        int yearsofexperience;
+
+    public:
+        Musician(); // a default constructor 
+        // a constructor that takes the instrument played and the years of experience
+        Musician(std::string instrument, int experience);      
+        std::string get_instrument();    // returns the instrument played
+        int get_experience();       // returns the number of years experience
+        ~Musician();                //A default destructor
+};
