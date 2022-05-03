90a7fdabbddb92d453b9c987f944f8c37ed10aee
Gerald Freislich <freislichg@gmail.com>
2022-05-03 13:38:56 +0930

    commit

--- attempt2/main-3-1.cpp
+++ attempt2/main-3-1.cpp
@@ -5,5 +5,20 @@ using std::cout; using std::string; using std::cin;
 int main(){
     Person player1;
     player1.move();
+
+    cout << "\nNew player" << "\n";
+    Player *player2 = new Person;
+    player2->move();
+
+    if(player1.win(player2)==false){
+        cout << "Player 1 lost";
+    }
+    else if(player1.win(player2) == true){
+        cout << "Player 2 lost";
+    }
+    else{
+        cout << "The values were not valid" << "\n";
+    }
+    
     return 0;
 }
