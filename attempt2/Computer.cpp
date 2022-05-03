64661d4098c548aeb5df540a70f014293ce92cb2
Gerald Freislich <freislichg@gmail.com>
2022-05-03 13:48:05 +0930

    commit

---
+++ attempt2/Computer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Computer.h"
+
+Computer::Computer(string letter){                       //set what move the computer will
+                                                            //make (rock, paper, or scissors)
+                                                            //if the input is not r, R, p, P, s, S or
+                                                            //a string starting with one of these letters,
+                                                           //set the move to the default ‘r’
+    if(letter != "r" && letter != "R" && letter != "s" && letter != "S" && letter != "p" && letter != "P"){
+        lastmove = 'r';
+    }
+    else{
+        lastmove = letter[0];
+    }
+    allmoves += lastmove;
+}
+
+string Computer::getMoves(){                              //returns all moves stored in a string
+    return allmoves;
+}
+
+void Computer::move(){                                       //increments number of moves made
+    for(int i = 0; i < numberofmoves; i++){
+        allmoves += allmoves[allmoves.size()-1];
+    }
+}
--- attempt2/Computer.h
+++ attempt2/Computer.h
@@ -1,12 +1,26 @@
 #include <iostream>
-#include "Player.h"
 using std::cout; using std::string; using std::cin;
+#include "Player.h"
 
 #ifndef COMPUTER_H
 #define COMPUTER_H
 
 class Computer: public Player {
+    private:
+       char lastmove;
+        string allmoves;
+        int numberofmoves;
 
+    public:
+        Computer(string letter);                       //set what move the computer will
+                                                            //make (rock, paper, or scissors)
+                                                            //if the input is not r, R, p, P, s, S or
+                                                            //a string starting with one of these letters,
+                                                           //set the move to the default ‘r’
+
+        string getMoves();                              //returns all moves stored in a string
+
+        void move();                                       //increments number of moves made
 };
 
 #endif
