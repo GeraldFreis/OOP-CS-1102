4613b00051d3f36a2e6ce900b163113c25417443
Gerald Freislich <freislichg@gmail.com>
2022-05-03 13:40:34 +0930

    commit

---
+++ attempt2/Computer.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Player.h"
+using std::cout; using std::string; using std::cin;
+
+#ifndef COMPUTER_H
+#define COMPUTER_H
+
+class Computer: public Player {
+
+};
+
+#endif
