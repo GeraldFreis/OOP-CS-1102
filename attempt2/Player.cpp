a1395dcc5899226d31e9cdba1efb44b4be18d64e
Gerald Freislich <freislichg@gmail.com>
2022-05-03 13:23:08 +0930

    safe

--- attempt2/Player.cpp
+++ attempt2/Player.cpp
@@ -10,4 +10,21 @@ char Player::getMove(){ //returns the most recent move made
 }
 bool Player::win(Player * opponent){ //compares players’ moves to see who wins
     char opplastmove = opponent->getMove();
+
+    // comparing the players
+    if((lastmove == 'r' || lastmove == 'R') && (opplastmove == 's' || opplastmove == 'S')){
+        return true;
+    }
+    else if((lastmove == 'p' || lastmove == 'P') && (opplastmove == 's' || opplastmove == 'S')){
+        return false;
+    }
+    else if((lastmove == 's' || lastmove == 'S') && (opplastmove == 'r' || opplastmove == 'R')){
+        return false;
+    }
+    else if((lastmove == 'r' || lastmove == 'R') && (opplastmove == 'p' || opplastmove == 'P')){
+        return true;
+    }
+    else{
+        cout << "Not a valid combination" << "\n";
+    }
 }
