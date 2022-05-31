a9c86137434215e1e53e687c984af55d94e3598e
Gerald Freislich <freislichg@gmail.com>
2022-05-31 10:18:13 +0930

    safe

--- animal.h
+++ animal.h
@@ -8,7 +8,7 @@ class animal{
     public:
         animal(string n, int v) ;  // creates an animal with name n and body volume v.
                             // animals are allocated a unique ID on creation
-        static int id;
+        
         void set_name(string n);
         void set_volume(int v);
         virtual string get_name() = 0;
@@ -19,6 +19,7 @@ class animal{
         string name ;              // the animal's name
         int animalID ;             // the animal's unique ID
         int volume ;               // the volume of the animal's body
+        static int id;
 };
 
 
--- zoo.cpp
+++ zoo.cpp
@@ -1,15 +1,16 @@
 #include "zoo.h"
 
 zoo::zoo(string n,int cows,int lions){      // create a zoo with the given number of cows and lions
-    *animals = new animal[cows+lions];
+    // *animals = new animal[cows+lions];
 
     for(int i = 0; i < cows; i++){
-        *animals[i] = vegie("daisy", 100);
+        animals[i] = new vegie("daisy", 100);
     }
 
     for(int i = 0; i < lions; i++){
-        *animals[i] = hunter("clarence", 50);
+        animals[i] = new hunter("clarence", 50);
     }
+    
     name = n;
     number_of_animals = cows + lions;
 
