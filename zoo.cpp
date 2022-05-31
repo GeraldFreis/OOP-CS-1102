9c9b8d8c68a8f93b165df4b5ec47e85e77f73bd2
Gerald Freislich <freislichg@gmail.com>
2022-05-31 10:27:27 +0930

    safe

--- zoo.cpp
+++ zoo.cpp
@@ -1,19 +1,17 @@
 #include "zoo.h"
 
 zoo::zoo(string n,int cows,int lions){      // create a zoo with the given number of cows and lions
-    // *animals = new animal[cows+lions];
-
+    animal **animal_s = new animal*[cows+lions];
     for(int i = 0; i < cows; i++){
         animals[i] = new vegie("daisy", 100);
     }
 
-    for(int i = 0; i < lions; i++){
+    for(int i = cows; i < (cows+lions); i++){
         animals[i] = new hunter("clarence", 50);
     }
-    
+
     name = n;
     number_of_animals = cows + lions;
-
 }
 
 string zoo::get_name(){
